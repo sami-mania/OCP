585d5f516d327d1c7673c5f08772fa36f4a51a5a
sami-mania <SAMI@192.168.178.18>
2016-03-12 21:53:42 +0100

    files modified in the package  "nl.cimsolutions.ocp.chapter13"

--- src/nl/cimsolutions/ocp/chapter13/ThreadA.java
+++ src/nl/cimsolutions/ocp/chapter13/ThreadA.java
@@ -10,10 +10,11 @@ package nl.cimsolutions.ocp.chapter13;
  * @author S.Rahman
  */
 public class ThreadA {
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         ThreadB b = new ThreadB();
         b.start();
-        synchronized(b){
+        synchronized (b) {
             try {
                 System.out.println("Waiting for b to complete......");
                 b.wait();
@@ -24,17 +25,18 @@ public class ThreadA {
     }
 }
 
-class ThreadB extends Thread{
+class ThreadB extends Thread {
+
     int total;
 
     @Override
     public void run() {
 //        super.run(); //To change body of generated methods, choose Tools | Templates.
-        synchronized(this){
-            for(int i=0;i<100;i++){
+        synchronized (this) {
+            for (int i = 0; i < 100; i++) {
                 total += i;
-            }            
+            }
             notify();
         }
-    }  
+    }
 }
