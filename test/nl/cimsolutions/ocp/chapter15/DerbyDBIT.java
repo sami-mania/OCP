4cd5e48b13e86df8bac9ee113620b98556b0ba82
S.Rahman <S.Rahman@Computer0131.CIMSOLUTIONS.local>
2016-03-16 17:07:52 +0100

    file modified for test cases in the package in nl.cimsolutions.ocp.chapter13

--- test/nl/cimsolutions/ocp/chapter15/DerbyDBIT.java
+++ test/nl/cimsolutions/ocp/chapter15/DerbyDBIT.java
@@ -47,6 +47,5 @@ public class DerbyDBIT {
         DerbyDB.main(args);
         // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
-    }
-    
+    }   
 }
