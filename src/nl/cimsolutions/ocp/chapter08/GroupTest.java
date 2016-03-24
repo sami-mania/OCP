0597104f403c27fe0484bc6b40f42d4733bbbc6a
S.Rahman <S.Rahman@Computer0131.CIMSOLUTIONS.local>
2016-03-24 17:31:39 +0100

    file added in the package in nl.cimsolutions.ocp.chapter08

---
+++ src/nl/cimsolutions/ocp/chapter08/GroupTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.cimsolutions.ocp.chapter08;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author S.Rahman
+ */
+public class GroupTest{
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile(args[0]);
+        Matcher m = p.matcher(args[1]);
+        System.out.println("\nSource: " + args[1]);
+        System.out.println("index: 0123456789\n");
+        System.out.println("Pattern: " + m.pattern());
+        while(m.find()){
+            System.out.println(m.start() + " " + m.group());
+        }
+        System.out.println("");
+   }
+}
