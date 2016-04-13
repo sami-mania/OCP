f32a51e81e5d6c091d51713f78c6e8e76bdd9603
S.Rahman <S.Rahman@Computer0131.CIMSOLUTIONS.local>
2016-04-13 17:13:52 +0200

    files modified in the package in nl.cimsolutions.ocp.chapter09

--- src/nl/cimsolutions/ocp/chapter09/PathInfo.java
+++ src/nl/cimsolutions/ocp/chapter09/PathInfo.java
@@ -13,6 +13,7 @@ import java.nio.file.Paths;
  * @author S.Rahman
  */
 public class PathInfo {
+
     public static void main(String[] args) {
         Path path = Paths.get("C:\\Users\\S.Rahman\\Documents\\NetBeansProjects\\OCP");
         System.out.println("getFileName: " + path.getFileName());
@@ -22,14 +23,56 @@ public class PathInfo {
         System.out.println("getRoot: " + path.getRoot());
         System.out.println("subpath(0,2): " + path.subpath(0, 2));
         System.out.println("toString: " + path.toString());
-        
+
         System.out.println("\n\n\n\n");
-        
-        int spaces=1;
-        Path path2 = Paths.get("Users","S.Rahman" , "Documents","NetBeansProjects","OCP");        
-        for(Path subpath: path2){
-            System.out.format("%" + spaces + "s%s%n","", subpath);
-            spaces +=3;
+
+        int spaces = 1;
+        Path path2 = Paths.get("Users", "S.Rahman", "Documents", "NetBeansProjects", "OCP");
+        for (Path subpath : path2) {
+            System.out.format("%" + spaces + "s%s%n", "", subpath);
+            spaces += 3;
         }
+
+        //path normalization
+        System.out.println(Paths.get("/a/./b/./c").normalize());
+        System.out.println(Paths.get(".classpath").normalize());
+        System.out.println(Paths.get("/a/b/c/..").normalize());
+        System.out.println(Paths.get("../a/b/c").normalize());
+        System.out.println("\n\n\n");
+        
+        //resolving a path
+        Path absolute = Paths.get("/home/java");
+        Path relative = Paths.get("dir");
+        Path file = Paths.get("Model.pdf");
+        System.out.println("\n\n\n");
+        
+        System.out.println("1: " + absolute.resolve(relative));
+        System.out.println("2: " + absolute.resolve(file));
+        System.out.println("3: " + relative.resolve(file));
+        System.out.println("4: " + relative.resolve(absolute));     //BAD example
+        System.out.println("5: " + file.resolve(absolute));         //BAD example
+        System.out.println("6: " + file.resolve(relative));     //BAD example
+        System.out.println("\n\n");
+        
+        
+        //Relativizing a Path
+        Path dir = Paths.get("/home/java");
+        Path music = Paths.get("/home/java/country/Swift.mp3");
+        Path mp3 = dir.relativize(music);
+        System.out.println(mp3);
+        
+        System.out.println("\n\n");
+        Path absolute1 = Paths.get("/home/java");
+        Path absolute2 = Paths.get("/usr/local");
+        Path absolute3 = Paths.get("/home/java/temp/music.mp3");
+        Path relative1 = Paths.get("temp");
+        Path relative2 = Paths.get("temp/music.pdf");
+        
+        System.out.println("1: " + absolute1.relativize(absolute3));
+        System.out.println("2: " + absolute3.relativize(absolute1));
+        System.out.println("3: " + absolute1.relativize(absolute2));
+        System.out.println("4: " + relative1.relativize(relative2));
+        System.out.println("5: " + absolute1.relativize(relative1));          //BAD
+        
     }
 }
