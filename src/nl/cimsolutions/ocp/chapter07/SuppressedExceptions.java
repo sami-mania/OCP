225021a40e006b7846079db2b0116aa3d9fde88e
S.Rahman <S.Rahman@Computer0131.CIMSOLUTIONS.local>
2016-07-07 17:30:45 +0200

    files added in the package in nl.cimsolutions.ocp.chapter07

---
+++ src/nl/cimsolutions/ocp/chapter07/SuppressedExceptions.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.cimsolutions.ocp.chapter07;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author S.Rahman
+ */
+public class SuppressedExceptions {
+    public static void main(String[] args) {
+        try(One one = new One()){
+            throw new Exception("try");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            for( Throwable t: e.getSuppressed()){
+                System.out.println("suppressed: " + t);
+            }
+        }
+    }
+}
+
+class One implements AutoCloseable{
+    @Override
+    public void close() throws Exception {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new IOException("Closing");
+    }
+}
+
